811e1b266216a4c1bc692ccc1e7352a806a3bf3c
mahinshabani <mahin.shabani94@gmail.com>
2022-07-20 18:12:58 +0430

    project Section from index move to partial Projects

---
+++ Devcard_MVC/Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Devcard_MVC.Models
+{
+    public class Project
+    {
+        public long Id { get; set; }
+        public String Image { get; set; }
+        public string Name { get; set; }
+        public string Client { get; set; }
+    }
+    
+    
+}
